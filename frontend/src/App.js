815e03884fc6143213b992888a4e03140e2297c3
dineth Dilshan <E2145112@bit.uom.lk>
2023-08-30 09:27:04 +0530

    Front end 1st stage done
    One user can use this

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,25 @@
 import { useEffect, useState } from "react";
 import ToDo from "./components/ToDo";
-import { getAllToDo } from "./utils/HandelAPI";
+import { getAllToDo, addToDo, updateToDo, deleteToDo } from "./utils/HandelAPI";
 
 function App() {
   const [toDo, setToDo] = useState([]);
 
+  const [text, setText] = useState("");
+  const [usersel, setUsersel] = useState("");
+
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [toDoId, setToDoId] = useState("");
+
   useEffect(() => {
     getAllToDo(setToDo);
   }, []);
+
+  const updateMode = (_id, text) => {
+    setIsUpdating(true);
+    setText(text);
+    setToDoId(_id);
+  };
 
   return (
     <div className="App">
@@ -15,14 +27,41 @@ function App() {
         <h1>ToDo App</h1>
 
         <div className="top">
-          <input type="text" placeholder="Add ToDos..." />
+          <input
+            type="text"
+            placeholder="Add ToDos..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Name"
+            value={usersel}
+            onChange={(e) => setUsersel(e.target.value)}
+          />
 
-          <div className="add">Add</div>
+          <div
+            className="add"
+            onClick={
+              isUpdating
+                ? () =>
+                    updateToDo(toDoId, text, setToDo, setText, setIsUpdating)
+                : () => addToDo(text, setText, setToDo, usersel, setUsersel)
+            }
+          >
+            {isUpdating ? "Update" : "Add"}
+          </div>
         </div>
 
         <div className="list">
           {toDo.map((item) => (
-            <ToDo key={item._id} text={item.text} />
+            <ToDo
+              key={item._id}
+              text={item.text}
+              usersel={item.usersel}
+              updateMode={() => updateMode(item._id, item.text)}
+              deleteToDo={() => deleteToDo(item._id, setToDo)}
+            />
           ))}
         </div>
       </div>
--- frontend/src/utils/HandelAPI.js
+++ frontend/src/utils/HandelAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:5000/";
+const baseUrl = "http://localhost:5000";
 
 const getAllToDo = (setToDo) => {
   axios.get(baseUrl).then(({ data }) => {
@@ -9,4 +9,36 @@ const getAllToDo = (setToDo) => {
   });
 };
 
-export { getAllToDo };
+const addToDo = (text, setText, setToDo, usersel, setUsersel) => {
+  axios
+    .post(`${baseUrl}/save`, { text, usersel })
+    .then((data) => {
+      console.log(data);
+      setText("");
+      setUsersel("");
+      getAllToDo(setToDo);
+    })
+    .catch((err) => console.log(err));
+};
+const updateToDo = (toDoId, text, setToDo, setText, setIsUpdating) => {
+  axios
+    .post(`${baseUrl}/update`, { _id: toDoId, text })
+    .then((data) => {
+      setText("");
+      setIsUpdating(false);
+      getAllToDo(setToDo);
+    })
+    .catch((err) => console.log(err));
+};
+
+const deleteToDo = (_id, setToDo) => {
+  axios
+    .post(`${baseUrl}/delete`, { _id })
+    .then((data) => {
+      console.log(data);
+      getAllToDo(setToDo);
+    })
+    .catch((err) => console.log(err));
+};
+
+export { getAllToDo, addToDo, updateToDo, deleteToDo };
